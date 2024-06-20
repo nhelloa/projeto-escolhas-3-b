421639d9b66912c7aa7ed7ae2dc8c5c36a4103a8
Helloa <160602656+nhelloa@users.noreply.github.com>
2024-06-20 08:30:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativa = document.querySelector9('.caix-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativa: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativa: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+
+    {
+        enunciado: "Pergunta 3",
+        alternativa: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativa: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativa: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+]
